73e4430d9753b426a61fbb217cbce4795705f880
Kishor Joshi <kishorjoshiabd@gmail.com>
2025-11-17 03:55:57 +0530

    setting up tailwind and font for the project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <div>Task Manager App</div>;
+  return <div className="text-blue-500 text-3xl">Task Manager App</div>;
 };
 
 export default App;
